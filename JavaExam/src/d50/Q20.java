ab750d2dea3f6a07f36568684c7615e34e4ba645
itc-s25002 <s25002@std.it-college.ac.jp>
2026-06-05 10:21:03 +0900

    Q20

--- JavaExam/src/d50/Q20.java
+++ JavaExam/src/d50/Q20.java
@@ -3,7 +3,7 @@ package d50;
 public class Q20 {
     public static void main(String[] args) {
         int num = 1234;
-        String scale = num > 1000 ?  "Narrow" : "Wide";{
+        String scale = num < 1000 ? "Wide" : "Narrow";
 
         System.out.println(scale);
     }
