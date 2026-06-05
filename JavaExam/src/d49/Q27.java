dba2c98b48febb2469b5e4e82efa6c3af92c3a6d
itc-s25002 <s25002@std.it-college.ac.jp>
2026-06-05 09:32:30 +0900

    Q27

---
+++ JavaExam/src/d49/Q27.java
@@ -0,0 +1,12 @@
+package d49;
+
+public class Q27 {
+    static void main(String[] args) {
+       int[] numbers = {10,20,30};
+       int sum = 0;
+       for (int num : numbers) {
+           sum += num;
+       }
+       System.out.println(sum);
+    }
+}
