9a538792ba1079e08470488f1eb3b8aae2ad6e35
itc-s25002 <s25002@std.it-college.ac.jp>
2026-06-05 11:18:03 +0900

    Q26

---
+++ JavaExam/src/d50/Q24.java
@@ -0,0 +1,10 @@
+package d50;
+
+public class Q24 {
+    public static void main(String[] args) {
+        String[] colors ={"red","blue","yellow"};
+        for (String c : colors) {
+            System.out.println(c);
+        }
+    }
+}
