00608d6f80b186168f644c68d4590d68508bc0b7
itc-s25002 <s25002@std.it-college.ac.jp>
2026-06-05 10:49:33 +0900

    Q23

--- JavaExam/src/d50/Q23.java
+++ JavaExam/src/d50/Q23.java
@@ -4,11 +4,11 @@ public class Q23 {
     String code;
     public static void main(String[] args) {
         Q23 obj = new Q23();
-        obj.setCode("Code");
+        System.out.println(obj.setCode("Code"));
 
     }
-    void setCode(String c){
+    String setCode(String c){
         code = c;
-        System.out.println(code);
+        return c;
     }
 }
